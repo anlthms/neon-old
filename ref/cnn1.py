367d911694124c893ccb97988cb23d6f93f702aa
Anil Thomas <anil@nervanasys.com>
2014-05-28 13:28:41 -0700

    CNN version 1

---
+++ ref/cnn1.py
@@ -0,0 +1,218 @@
+"""
+CNN using basic operations - version 1.
+
+XXX: Currently broken - the training error refuses to go down
+     beyond a certain point.
+ 
+"""
+
+import math
+import cPickle
+import numpy as np
+from scipy.signal import fftconvolve 
+
+def cross_entropy(outputs, targets):
+    return np.mean(-targets * np.log(outputs) - \
+                   (1 - targets) * np.log(1 - outputs))
+def logistic(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+def logistic_prime(z):
+    y = logistic(z)
+    return y * (1.0 - y) 
+
+def get_prime(func):
+    if func == logistic:
+        return logistic_prime
+
+def ce_de(outputs, targets):
+    return (outputs - targets) / (outputs * (1.0 - outputs)) 
+
+def init_weights(nrows, ncols):
+    return 0.01 * np.random.randn(nrows, ncols)
+
+def error_rate(preds, labels):
+    return 100.0 * np.mean(np.not_equal(preds, labels))
+
+class Type:
+    fcon = 0    # Fully connected
+    conv = 1    # Convolutional
+    pool = 2    # Max-pooling
+
+class Layer:
+    def __init__(self, nin, nout, g):
+        self.weights = init_weights(nin, nout)
+        self.g = g
+        self.gprime = get_prime(g)
+        self.nout = nout
+        
+    def fprop(self, inputs):
+        self.z = np.dot(inputs, self.weights)
+        self.y = self.g(self.z)
+        return self.y
+
+    def bprop(self, error):
+        self.delta = error * self.gprime(self.z)
+
+    def update(self, inputs, epsilon):
+        self.weights -= epsilon * np.dot(inputs.T, self.delta)
+
+class ConvLayer:
+    def __init__(self, nin, g, ishape, fshape, nfilt):
+        self.iheight, self.iwidth = ishape 
+        self.fheight, self.fwidth = fshape
+
+        fmheight = self.iheight - self.fheight + 1
+        fmwidth = self.iwidth - self.fwidth + 1
+        self.fmsize = fmheight * fmwidth
+        self.nout = self.fmsize * nfilt
+
+        self.nfilt = nfilt
+        self.fsize = self.fheight * self.fwidth 
+        self.weights = init_weights(nfilt, self.fsize)
+        self.g = g
+        self.gprime = get_prime(g)
+        
+    def fprop(self, inputs):
+        self.z = np.zeros((inputs.shape[0], self.nout))
+        for i in range(self.nfilt):
+            filt = self.weights[i]
+            src = 0
+            for dst in range(self.fmsize):
+                # This loop can probably be optimized. I don't want to spend
+                # time on it if we are going to use a DSP for convolution.
+                colinds = []
+                for row in range(self.fheight):
+                    start = src + row * self.iwidth
+                    colinds += range(start, start + self.fwidth) 
+                self.z[:, (i * self.fmsize + dst)] = np.dot(inputs[:, colinds], filt)  
+                src += 1
+                if (src % self.iwidth + self.fwidth) > self.iwidth:
+                    src += self.fwidth - 1
+
+        self.y = self.g(self.z)
+        return self.y
+
+    def fpropfft_unused(self, inputs):
+        self.z = np.zeros((inputs.shape[0], self.nout))
+        for i in range(self.nfilt):
+            filt = self.weights[i].reshape((self.fheight, self.fwidth))
+            for j in range(inputs.shape[0]):
+                self.z[j, (i * self.fmsize):(i + 1) * self.fmsize] = \
+                        fftconvolve(inputs[j,:].reshape((self.iheight,
+                        self.iwidth)), filt, mode='valid').flatten()
+        self.y = self.g(self.z)
+        return self.y
+
+    def fpropfft3d_unused(self, inputs):
+        self.z = np.zeros((inputs.shape[0], self.nout))
+        for i in range(self.nfilt):
+            filt = self.weights[i].reshape((1, self.fheight, self.fwidth))
+            self.z[:, (i * self.fmsize):(i + 1) * self.fmsize] = \
+                        fftconvolve(inputs.reshape((inputs.shape[0],
+                        self.iheight, self.iwidth)), filt,
+                        mode='valid').reshape((inputs.shape[0], self.fmsize))
+        self.y = self.g(self.z)
+        return self.y
+
+    def bprop(self, error):
+        self.delta = error * self.gprime(self.z)
+
+    def update_unused(self, inputs, epsilon):
+        for i in range(self.nfilt):
+            wsums = np.zeros(self.weights[i].shape) 
+            src = 0
+            # Note that this can also be replaced with a call to
+            # fftconvolve (like in fpropfft).
+            for dst in range(self.fmsize):
+                colinds = []
+                for row in range(self.fheight):
+                    start = src + row * self.iwidth
+                    colinds += range(start, start + self.fwidth) 
+                wsums += np.dot(inputs[:, colinds].T, self.delta[:, dst])
+                src += 1
+                if (src % self.iwidth + self.fwidth) > self.iwidth:
+                    src += self.fwidth - 1
+
+            self.weights[i] -= epsilon * (wsums / self.fmsize) 
+
+    def update(self, inputs, epsilon):
+        for i in range(self.nfilt):
+            wsums = np.zeros(self.fsize) 
+            src = 0
+            for dst in range(self.fmsize):
+                for w in range(self.fsize):
+                    colind = src + w 
+                    wsums[w] += np.dot(inputs[:, colind].T, self.delta[:, dst])
+                src += 1
+                if (src % self.iwidth + self.fwidth) > self.iwidth:
+                    src += self.fwidth - 1
+
+            self.weights[i] -= epsilon * (wsums / self.fmsize) 
+
+class MultilayerPerceptron:
+    def fit(self, inputs, targets, nepochs, epsilon, confs, de):
+        nin = inputs.shape[1]
+        self.de = de
+        self.nlayers = len(confs)
+        self.layers = []
+        for i in range(self.nlayers):
+            layer = self.lcreate(nin, confs[i])
+            self.layers.append(layer)
+            nin = layer.nout
+
+        for epoch in range(nepochs): 
+            self.fprop(inputs)
+            self.bprop(inputs, targets)
+            self.update(inputs, epsilon) 
+            error = cross_entropy(self.layers[-1].y, targets)
+            print 'epoch ' + str(epoch) + ' training error ' + \
+                   str(round(error, 5))
+
+    def predict(self, inputs):
+        outputs = self.fprop(inputs)
+        preds = np.argmax(outputs, axis=1) 
+        return preds
+
+    def lcreate(self, nin, conf):
+        if conf[0] == Type.fcon:
+            return Layer(nin, nout=conf[2], g=conf[1])
+
+        if conf[0] == Type.conv:
+            return ConvLayer(nin, g=conf[1], ishape = conf[2],
+                             fshape=conf[3], nfilt=conf[4])
+
+    def fprop(self, inputs):
+        y = inputs
+        for layer in self.layers: 
+            y = layer.fprop(y)
+        return y
+
+    def bprop(self, inputs, targets):
+        i = self.nlayers - 1
+        error = self.de(self.layers[i].y, targets)
+        self.layers[i].bprop(error)
+        while i > 0:
+            error = np.dot(self.layers[i].delta, self.layers[i].weights.T) 
+            i -= 1 
+            self.layers[i].bprop(error)
+
+    def update(self, inputs, epsilon):
+        self.layers[0].update(inputs, epsilon)
+        for i in range(1, self.nlayers):
+            self.layers[i].update(self.layers[i - 1].y, epsilon)
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    trainData, unused1, trainTargets, testData, testLabels, unused2 = \
+            cPickle.load(open('smnist.pkl'))
+    net = MultilayerPerceptron()
+    net.fit(trainData, trainTargets, nepochs=10, epsilon=0.00002,
+            de=ce_de,
+            confs=[(Type.conv, logistic, (28, 28), (5, 5), 1),
+                   (Type.fcon, logistic, 50),
+                   (Type.fcon, logistic, trainTargets.shape[1])])
+    
+    preds = net.predict(testData)
+    errorRate = error_rate(preds, testLabels)
+    print 'test error rate ' + str(round(errorRate, 2)) + '%' 
