Fix bug in logistic derivative transform.

mylearn/transforms/tests/test_logistic.py
@@ -1,5 +1,3 @@
-from math import exp
-
 from nose.plugins.attrib import attr
 import numpy as np
 
@@ -30,7 +28,7 @@ def test_logistic_CudamatTensor():
 def test_logistic_derivative_NumpyTensor():
     inputs = np.array([0, 1, -2]).reshape((3, 1))
     outputs = 1.0 / (1.0 + np.exp(-inputs))
-    outpus = outputs * (1.0 - outputs)
+    outputs = outputs * (1.0 - outputs)
     temp = Numpy.zeros(inputs.shape)
     logistic_derivative(Numpy, NumpyTensor(inputs), temp)
     assert_tensor_near_equal(NumpyTensor(outputs), temp)
@@ -41,7 +39,7 @@ def test_logistic_derivative_CudamatTensor():
     from mylearn.backends._cudamat import Cudamat, CudamatTensor
     inputs = np.array([0, 1, -2]).reshape((3, 1))
     outputs = 1.0 / (1.0 + np.exp(-inputs))
-    outpus = outputs * (1.0 - outputs)
+    outputs = outputs * (1.0 - outputs)
     c = Cudamat(rng_seed=0)
     temp = c.zeros(inputs.shape)
     logistic_derivative(c, CudamatTensor(inputs), temp)