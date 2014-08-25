a0f9f645e32ba9043fdf2a357979c116b1f36745
Scott Leishman <scott@nervanasys.com>
2014-08-24 21:39:42 -0700

    Converted Google Brain autoencoder to new YAML object format.

--- mylearn/models/gb.py
+++ mylearn/models/gb.py
@@ -5,11 +5,8 @@ Contains code to train Google Brain models and run inference.
 import logging
 import math
 
-from mylearn.models.layer import LayerWithNoBias
 from mylearn.models.layer import LocalFilteringLayer
-from mylearn.models.layer import L2PoolingLayer, LCNLayer
 from mylearn.models.mlp import MLP
-from mylearn.util.factory import Factory
 
 logger = logging.getLogger(__name__)
 
@@ -21,35 +18,25 @@ class GB(MLP):
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        if isinstance(self.cost, str):
-            self.cost = Factory.create(type=self.cost)
-        if isinstance(self.pretraining_cost, str):
-            self.pretraining_cost = Factory.create(type=self.pretraining_cost)
 
     def pretrain(self, datasets):
         logger.info('commencing unsupervised pretraining')
         inputs = datasets[0].get_inputs(train=True)['train']
         nrecs, nin = inputs.shape
-        self.backend = datasets[0].backend
-        self.backend.rng_init()
         self.nlayers = len(self.layers)
         if 'batch_size' not in self.__dict__:
             self.batch_size = nrecs
         self.trainable_layers = []
-        layers = []
         self.temp = []
-        for i in xrange(self.nlayers):
-            layer = self.lcreate(self.backend, nin, self.layers[i], i)
+        for ind in xrange(len(self.layers)):
+            layer = self.layers[ind]
             if isinstance(layer, LocalFilteringLayer):
-                self.trainable_layers.append(i)
+                self.trainable_layers.append(ind)
                 self.temp.append(self.backend.zeros((self.batch_size, nin)))
-            logger.info('created layer:\n\t%s' % str(layer))
-            layers.append(layer)
             nin = layer.nout
-        self.layers = layers
 
         num_batches = int(math.ceil((nrecs + 0.0) / self.batch_size))
-        for ind in range(len(self.trainable_layers)):
+        for ind in xrange(len(self.trainable_layers)):
             layer = self.layers[self.trainable_layers[ind]]
             for epoch in xrange(self.num_pretraining_epochs):
                 error = 0.0
@@ -59,7 +46,7 @@ class GB(MLP):
                     output = inputs[start_idx:end_idx]
                     # Forward propagate the input all the way to
                     # the layer that we are pretraining.
-                    for i in range(self.trainable_layers[ind]):
+                    for i in xrange(self.trainable_layers[ind]):
                         self.layers[i].fprop(output)
                         output = self.layers[i].output
 
@@ -78,7 +65,6 @@ class GB(MLP):
         inputs = datasets[0].get_inputs(train=True)['train']
         targets = datasets[0].get_targets(train=True)['train']
         nrecs, nin = inputs.shape
-        self.backend.rng_init()
         tempbuf = self.backend.zeros((self.batch_size, targets.shape[1]))
         self.temp = [tempbuf, tempbuf.copy()]
 
@@ -102,49 +88,3 @@ class GB(MLP):
     def fit(self, datasets):
         self.pretrain(datasets)
         self.train(datasets)
-
-    def lcreate(self, backend, nin, conf, pos):
-        if conf['connectivity'] == 'full':
-            activation = Factory.create(type=conf['activation'])
-            return LayerWithNoBias(conf['name'], backend,
-                                   self.batch_size, pos,
-                                   self.learning_rate,
-                                   nin,
-                                   nout=conf['num_nodes'],
-                                   activation=activation,
-                                   weight_init=conf['weight_init'])
-        if conf['connectivity'] == 'lf':
-            input_shape = conf['input_shape'].split()
-            ifmshape = (int(input_shape[0]), int(input_shape[1]))
-            filter_shape = conf['filter_shape'].split()
-            fshape = (int(filter_shape[0]), int(filter_shape[1]))
-            return LocalFilteringLayer(conf['name'], backend,
-                                       self.batch_size, pos,
-                                       self.learning_rate,
-                                       nifm=conf['num_input_channels'],
-                                       ifmshape=ifmshape,
-                                       fshape=fshape,
-                                       stride=conf['stride'],
-                                       weight_init=conf['weight_init'])
-        if conf['connectivity'] == 'l2pool':
-            input_shape = conf['input_shape'].split()
-            ifmshape = (int(input_shape[0]), int(input_shape[1]))
-            pooling_shape = conf['pooling_shape'].split()
-            pshape = (int(pooling_shape[0]), int(pooling_shape[1]))
-            return L2PoolingLayer(conf['name'], backend,
-                                  self.batch_size, pos,
-                                  nfm=conf['num_channels'],
-                                  ifmshape=ifmshape,
-                                  pshape=pshape,
-                                  stride=conf['stride'])
-        if conf['connectivity'] == 'lcn':
-            input_shape = conf['input_shape'].split()
-            ifmshape = (int(input_shape[0]), int(input_shape[1]))
-            filter_shape = conf['filter_shape'].split()
-            fshape = (int(filter_shape[0]), int(filter_shape[1]))
-            return LCNLayer(conf['name'], backend,
-                            self.batch_size, pos,
-                            nifm=conf['num_channels'],
-                            ifmshape=ifmshape,
-                            fshape=fshape,
-                            stride=conf['stride'])
