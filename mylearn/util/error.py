528bc6352c2282cf23c18ba342119b99c42ffe5c
Scott Leishman <scott@nervanasys.com>
2014-09-04 16:20:25 -0700

    Isolate checks for CUDA and related.  Fixes MYL-50.
    Also added some minor refactoring and style corrections.

---
+++ mylearn/util/error.py
@@ -0,0 +1,11 @@
+"""
+Application specific Error and exception definitions.
+"""
+
+
+class TooSlowToImplementError(Exception):
+
+    """
+    Used to indicate types of operations that would take too long to run.
+    """
+    pass
