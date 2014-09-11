fc66463311c5903adc7bd20ec4935bee3ce0f544
Anil Thomas <anil@nervanasys.com>
2014-09-11 07:38:25 -0700

    Spot-checking of classification accuracy after pretraining.

--- mylearn/models/gb.py
+++ mylearn/models/gb.py
@@ -45,7 +45,7 @@ class GB(MLP):
                     trcost += rcost
                     tspcost += spcost
                 tcost = trcost + tspcost
-                logger.info('epoch: %d, cost: %0.2f, %0.2f, %0.2f' %
+                logger.info('epoch: %d, cost: %0.2f + %0.2f = %0.2f' %
                             (epoch, trcost / num_batches,
                              tspcost / num_batches, tcost / num_batches))
                 if self.visualize:
@@ -73,7 +73,7 @@ class GB(MLP):
                 start_idx = batch * self.batch_size
                 end_idx = min((batch + 1) * self.batch_size, self.nrecs)
                 self.fprop(inputs[start_idx:end_idx])
-                if epoch <= 0:
+                if epoch < self.num_initial_epochs:
                     self.bprop_last(targets[start_idx:end_idx],
                                     inputs[start_idx:end_idx],
                                     epoch, self.momentum)
@@ -85,8 +85,63 @@ class GB(MLP):
                                                   self.layers[-1].output,
                                                   targets[start_idx:end_idx],
                                                   self.temp)
-            logger.info('epoch: %d, total training error: %0.5f' %
+            logger.info('epoch: %d, training error: %0.5f' %
                         (epoch, error / num_batches))
+
+    def check_node_predictions(self, inputs, targets, node, cls):
+        """
+        Spot-check the classification accuracy of an output neuron
+        for the given class.
+        """
+        from sklearn import metrics
+        num_batches = int(math.ceil((self.nrecs + 0.0) / self.batch_size))
+        labels = self.backend.zeros((targets.shape[0]), dtype=int)
+        labels[targets[:, cls] == 0] = 0
+        labels[targets[:, cls] == 1] = 1
+        auc = 0.0
+        for batch in xrange(num_batches):
+            start_idx = batch * self.batch_size
+            end_idx = min((batch + 1) * self.batch_size, self.nrecs)
+            self.fprop(inputs[start_idx:end_idx])
+            # Get the output of the last LCN layer.
+            pred = self.layers[-2].output[:, node]
+            auc += metrics.roc_auc_score(
+                labels[start_idx:end_idx].raw(), pred.raw())
+        auc /= num_batches
+        return auc
+
+    def check_predictions(self, inputs, targets, test_inputs, test_targets):
+        """
+        Check the classification accuracy of output neurons.
+        """
+        from sklearn import metrics
+        num_batches = int(math.ceil((self.nrecs + 0.0) / self.batch_size))
+        labels = self.backend.zeros((targets.shape[0]), dtype=int)
+        sum = 0.0
+        for cls in xrange(targets.shape[1]):
+            labels[targets[:, cls] == 0] = 0
+            labels[targets[:, cls] == 1] = 1
+            auc = self.backend.zeros((self.layers[-2].output.shape[1]))
+            for batch in xrange(num_batches):
+                start_idx = batch * self.batch_size
+                end_idx = min((batch + 1) * self.batch_size, self.nrecs)
+                self.fprop(inputs[start_idx:end_idx])
+                # Get the output of the last LCN layer.
+                for node in xrange(auc.shape[0]):
+                    pred = self.layers[-2].output[:, node]
+                    auc[node] += metrics.roc_auc_score(
+                        labels[start_idx:end_idx].raw(), pred.raw())
+            auc /= num_batches
+            maxnode = self.backend.argmax(auc).raw()
+            maxauc = auc[maxnode]
+            # Check classification accuracy of the best neuron on the test set.
+            testauc = self.check_node_predictions(test_inputs, test_targets,
+                                                  maxnode, cls)
+            logger.info(
+                'class: %d best node: %d train auc: %.4f test auc: %.4f' %
+                (cls, maxnode, maxauc, testauc))
+            sum += maxauc
+        logger.info('average max auc %.4f' % (sum / targets.shape[1]))
 
     def bprop_last(self, targets, inputs, epoch, momentum):
         # Backprop on just the last layer.
@@ -110,12 +165,16 @@ class GB(MLP):
                 self.trainable_layers.append(ind)
             logger.info('created layer:\n\t%s' % str(layer))
 
+        targets = datasets[0].get_targets(train=True)['train']
         if self.pretraining:
             self.pretrain(inputs)
-        targets = datasets[0].get_targets(train=True)['train']
+            if self.visualize:
+                self.compute_optimal_stimulus()
+        if self.spot_check:
+            test_inputs = datasets[0].get_inputs(test=True)['test']
+            test_targets = datasets[0].get_targets(test=True)['test']
+            self.check_predictions(inputs, targets, test_inputs, test_targets)
         self.train(inputs, targets)
-        if self.visualize:
-            self.compute_optimal_stimulus()
 
     def normalize(self, data):
         norms = data.norm(axis=1)
