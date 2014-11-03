e89f7c145d9a533e16728161eb15b1861a1260b7
Alex Park <alex.park@gmail.com>
2014-11-03 11:30:29 -0800

    Fixed Learning Rule code to work correctly with AdaDelta and created some new examples.  Also changed cifar10 dist yaml file, but still not working with mlp using existing dist layers

--- neon/models/learning_rule.py
+++ neon/models/learning_rule.py
@@ -1,28 +1,32 @@
 """
-Classes used to control how updates are applied to coefficients
-i.e. how the learning should proceed.
+Generic updating class
 """
 
 import logging
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
 
 class LearningRule(object):
+
     """
-    Base object for applying a learning rule on the parameters to be updated
+    LearningRule object for applying learning rule on params to be updated
 
     Attributes:
         name (str): Used to identify this LearningRule when logging.
         backend (neon.backends.backend.Backend): underlying type for stored
-                                                 data parameters like weights.
+                                                    data parameters like
+                                                    weights.
         batch_size (int): Number of examples presented at this iteration
     """
-    def __init__(self, name, lr_params, param_dtype=None, gradient_dtype=None):
+
+    def __init__(self, name, lr_params, param_dtype=None,     gradient_dtype=None):
         self.name = name
         self.backend = lr_params['backend']
         self.param_dtype = param_dtype
         self.gradient_dtype = gradient_dtype
+
 
     def __str__(self):
         return ("LearningRule {upd_nm}: {upd_tp} upd_rl, "
@@ -39,12 +43,12 @@ class LearningRule(object):
     def apply_rule(self, params, updates, epoch):
         raise NotImplementedError()
 
-
 class GradientDescent(LearningRule):
+
     """
-    Vanilla gradient descent based update rule that can optionally support use
-    of weight decay.
+    Single NNet layer with no bias node
     """
+
     def __init__(self, name, lr_params, param_dtype=None, gradient_dtype=None):
         super(GradientDescent, self).__init__(name, lr_params)
         if 'learning_rate' in lr_params:
@@ -64,10 +68,11 @@ class GradientDescent(LearningRule):
 
 
 class GradientDescentPretrain(GradientDescent):
+
     """
-    Gradient descent based variant that also supports a separate learning
-    rate during pre-training.
+    Single NNet layer with no bias node
     """
+
     def __init__(self, name, lr_params, param_dtype=None, gradient_dtype=None):
         super(GradientDescentPretrain, self).__init__(name, lr_params)
         if 'pretrain_learning_rate' in lr_params:
@@ -85,16 +90,15 @@ class GradientDescentPretrain(GradientDescent):
             self.learning_rate = self.train_learning_rate
 
     def apply_rule(self, params, updates, epoch):
-        self.backend.multiply(updates, self.backend.wrap(self.learning_rate),
-                              out=updates)
+        self.backend.multiply(updates, self.backend.wrap(self.learning_rate), out=updates)
         self.backend.subtract(params, updates, out=params)
 
-
 class GradientDescentMomentum(GradientDescent):
+
     """
-    Gradient descent learning rate variant that supports different types of
-    momentum based updates
+    Single NNet layer with no bias node
     """
+
     def __init__(self, name, lr_params, param_dtype=None, gradient_dtype=None):
         super(GradientDescentMomentum, self).__init__(name, lr_params)
         if 'momentum_params' in lr_params:
@@ -105,9 +109,8 @@ class GradientDescentMomentum(GradientDescent):
         self.velocity_dtype = param_dtype
 
     def allocate_state(self, params):
-        if self.velocity is None:
-            self.velocity = self.backend.zeros(params.shape,
-                                               self.velocity_dtype)
+        if (self.velocity == None):
+            self.velocity = self.backend.zeros(params.shape, self.velocity_dtype)
 
     def apply_rule(self, params, updates, epoch):
         momentum_coef = self.get_momentum_coef(epoch)
@@ -164,18 +167,26 @@ class GradientDescentMomentum(GradientDescent):
         return coef
 
 
-# TODO:  Use the built-in ada-delta update funcs in the backends to make this
-# cleaner/faster
+# TODO:  Use the built-in ada-delta update funcs in the backends to make this cleaner/faster
 class AdaDelta(LearningRule):
+
     """
-    Adadelta based learning rule updates.  See Zeiler2012 for instance.
+    Single NNet layer with no bias node
     """
+
     def __init__(self, name, lr_params, param_dtype=None, gradient_dtype=None):
         super(AdaDelta, self).__init__(name, lr_params)
-        self.rho = rho
-        self.epsilon = epsilon
+        if 'rho' in lr_params:
+            self.rho = lr_params['rho']
+        else:
+            raise AttributeError("Missing required parameter rho")
+        if 'epsilon' in lr_params:
+            self.epsilon = lr_params['epsilon']
+        else:
+            raise AttributeError("Missing required parameter epsilon")
         self.exp_gradsq_dtype = param_dtype
         self.exp_deltsq_dtype = param_dtype
+        self.scratch_space_dtype = param_dtype
         self.lrates_dtype = param_dtype
         self.lrates_dtype = param_dtype
         self.exp_gradsq = None
@@ -184,17 +195,15 @@ class AdaDelta(LearningRule):
         self.scratch_space = None
 
     def allocate_state(self, params):
-        if self.exp_gradsq is None:
-            self.exp_gradsq = self.backend.zeros(params.shape,
-                                                 self.exp_gradsq_dtype)
-        if self.exp_deltsq is None:
-            self.exp_deltsq = self.backend.zeros(params.shape,
-                                                 self.exp_deltsq_dtype)
-        if self.lrates is None:
+
+        if (self.exp_gradsq == None):
+            self.exp_gradsq = self.backend.zeros(params.shape, self.exp_gradsq_dtype)
+        if (self.exp_deltsq == None):
+            self.exp_deltsq = self.backend.zeros(params.shape, self.exp_deltsq_dtype)
+        if (self.lrates == None):
             self.lrates = self.backend.zeros(params.shape, self.lrates_dtype)
-        if self.scratch_space is None:
-            self.scratch_space = self.backend.zeros(params.shape,
-                                                    self.scratch_space_dtype)
+        if (self.scratch_space == None):
+            self.scratch_space = self.backend.zeros(params.shape, self.scratch_space_dtype)
 
     def apply_rule(self, params, updates, epoch):
         # Accumulate E[Grad^2]
@@ -202,11 +211,9 @@ class AdaDelta(LearningRule):
                               out=self.exp_gradsq)
         self.backend.multiply(updates, updates,
                               out=self.scratch_space)
-        self.backend.multiply(self.scratch_space, self.backend.wrap(1.0 -
-                                                                    self.rho),
+        self.backend.multiply(self.scratch_space, self.backend.wrap(1.0-self.rho),
                               out=self.scratch_space)
-        self.backend.add(self.exp_gradsq, self.scratch_space,
-                         out=self.exp_gradsq)
+        self.backend.add(self.exp_gradsq, self.scratch_space, out=self.exp_gradsq)
 
         # Calculate Updates
         self.backend.add(self.exp_gradsq, self.backend.wrap(self.epsilon),
@@ -215,8 +222,7 @@ class AdaDelta(LearningRule):
                          out=self.lrates)
         self.backend.divide(self.lrates, self.scratch_space, out=self.lrates)
         self.backend.sqrt(self.lrates, out=self.lrates)
-        self.backend.multiply(self.lrates, self.backend.wrap(-1.0),
-                              out=self.lrates)
+        self.backend.multiply(self.lrates, self.backend.wrap(-1.0), out=self.lrates)
         self.backend.multiply(self.lrates, updates, out=self.lrates)
 
         # Accumulate E[Delt^2]
@@ -224,11 +230,9 @@ class AdaDelta(LearningRule):
                               out=self.exp_deltsq)
         self.backend.multiply(self.lrates, self.lrates,
                               out=self.scratch_space)
-        self.backend.multiply(self.scratch_space, self.backend.wrap(1.0 -
-                                                                    self.rho),
+        self.backend.multiply(self.scratch_space, self.backend.wrap(1.0-self.rho),
                               out=self.scratch_space)
-        self.backend.add(self.exp_deltsq, self.scratch_space,
-                         out=self.exp_deltsq)
+        self.backend.add(self.exp_deltsq, self.scratch_space, out=self.exp_deltsq)
 
         # Final update to the params
         self.backend.add(params, self.lrates, out=params)
