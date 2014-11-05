2702cd4de33fb4f923f8fef73fee8775bc9f8bb2
Arjun Bansal <arjun@nervanasys.com>
2014-11-05 00:20:08 -0800

    *** implements distributed MLP for layers with biases. closes MYL-98

--- neon/datasets/mnist.py
+++ neon/datasets/mnist.py
@@ -8,6 +8,7 @@ import logging
 import numpy
 import os
 import struct
+import numpy as np
 
 from neon.datasets.dataset import Dataset
 from neon.util.compat import PY3, MPI_INSTALLED
@@ -55,11 +56,41 @@ class MNIST(Dataset):
             if MPI_INSTALLED:
                 from mpi4py import MPI
                 self.comm = MPI.COMM_WORLD
-                # for now require that comm.size is a square and divides 28
+                # for now require that comm.size be square and sqrt() divide 28
                 if self.comm.size not in [1, 4, 16]:
                     raise AttributeError('MPI.COMM_WORLD.size not compatible')
             else:
                 raise AttributeError("dist_flag set but mpi4py not installed")
+
+    def adjust_for_dist(self):
+        comm_rank = self.comm.rank
+        self.dist_indices = []
+        img_width = 28
+        img_size = img_width ** 2
+
+        if self.dist_mode == 0:
+            # this requires comm_per_dim to be a square for now
+            self.comm_per_dim = int(np.sqrt(self.comm.size))
+            self.px_per_dim = img_width / self.comm_per_dim
+            r_i = []
+            c_i = []
+            # top left corner in 2-D image
+            for row in range(self.comm_per_dim):
+                for col in range(self.comm_per_dim):
+                    r_i.append(row * self.px_per_dim)
+                    c_i.append(col * self.px_per_dim)
+            for r in range(r_i[comm_rank], r_i[comm_rank] + self.px_per_dim):
+                self.dist_indices.extend(
+                    [r * img_width + x for x in range(
+                        c_i[comm_rank], c_i[comm_rank] + self.px_per_dim)])
+        elif self.dist_mode == 1:
+            start_idx = 0
+            for j in range(comm_rank):
+                start_idx += (img_size // self.comm.size +
+                              (img_size % self.comm.size > j))
+            nin = (img_size // self.comm.size +
+                   (img_size % self.comm.size > comm_rank))
+            self.dist_indices.extend(range(start_idx, start_idx + nin))
 
     def read_image_file(self, fname, dtype=None):
         """
@@ -70,32 +101,16 @@ class MNIST(Dataset):
             if magic != 2051:
                 raise ValueError('invalid MNIST image file: ' + fname)
             full_image = numpy.fromfile(f, dtype='uint8').reshape((num_images,
-                                                                   rows, cols))
+                                                                   rows*cols))
+
         if dtype is not None:
             dtype = numpy.dtype(dtype)
             full_image = full_image.astype(dtype)
             full_image /= 255.
 
         if self.dist_flag:
-            # read corresponding quadrant of the image
-            comm_rank = self.comm.rank
-            # todo: will change for different x/y dims for comm_per_dim
-            self.comm_per_dim = int(numpy.sqrt(self.comm.size))
-            px_per_dim = 28 / self.comm_per_dim
-            r_i = []
-            c_i = []
-            # top left corner in 2-D image
-            for row in range(self.comm_per_dim):
-                for col in range(self.comm_per_dim):
-                    r_i.append(row * px_per_dim)
-                    c_i.append(col * px_per_dim)
-            array = numpy.empty(
-                (num_images, px_per_dim, px_per_dim), dtype=dtype)
-            l_ptr = 0
-            for r in range(r_i[comm_rank], r_i[comm_rank] + px_per_dim):
-                array[:, l_ptr] = full_image[
-                    :, r, range(c_i[comm_rank], c_i[comm_rank] + px_per_dim)]
-                l_ptr += 1
+            self.adjust_for_dist()
+            array = full_image[:, self.dist_indices]
         else:
             array = full_image
 
@@ -140,18 +155,13 @@ class MNIST(Dataset):
                             for line in infile:
                                 outfile.write(line)
                 logger.info('loading: %s' % name)
-                if self.dist_flag:
-                    img_size = 784 / self.comm.size
-                else:
-                    img_size = 784
                 if 'images' in repo_file and 'train' in repo_file:
                     indat = self.read_image_file(repo_file, 'float32')
                     # flatten to 1D images
-                    indat = indat.reshape((60000, img_size))[train_idcs]
+                    indat = indat[train_idcs]
                     self.inputs['train'] = indat
                 elif 'images' in repo_file and 't10k' in repo_file:
                     indat = self.read_image_file(repo_file, 'float32')
-                    indat = indat.reshape((10000, img_size))
                     self.inputs['test'] = indat
                 elif 'labels' in repo_file and 'train' in repo_file:
                     indat = self.read_label_file(repo_file)[train_idcs]
