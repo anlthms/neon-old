f2431ea121792f00e726717eda75f0639fc44271
Anil Thomas <anil@nervanasys.com>
2014-11-06 06:49:24 -0800

    Add configuration file for sanity test.
    Connect all the stackable layers together and train this network on a toy dataset. Execute this test when "make test" is performed.

---
+++ neon/tests/test_layers.py
@@ -0,0 +1,17 @@
+"""
+Sanity test layers.
+"""
+import os
+import neon
+import logging
+from neon.util.persist import deserialize
+
+
+def test_layers():
+    dir = os.path.dirname(os.path.realpath(__file__))
+    experiment = deserialize(os.path.join(dir, 'sanity_layers.yaml'))
+    if hasattr(experiment, 'logging'):
+        logging.basicConfig(**experiment.logging)
+
+    experiment.run()
+    assert experiment.model.result == 0.34375
