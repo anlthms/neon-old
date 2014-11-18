cdaad12eb4322b92a0fb14659a93a1745473277e
Scott Leishman <scott@nervanasys.com>
2014-11-17 17:59:41 -0800

    revert previous gpu unit test change

--- neon/backends/tests/test_gputensor.py
+++ neon/backends/tests/test_gputensor.py
@@ -46,7 +46,7 @@ class TestGPUTensor(object):
     @attr('cuda')
     def test_str(self):
         tns = GPUTensor([[1, 2], [3, 4]])
-        assert str(tns) == "[[ 1.  2.]\n [ 3.  4.]]"
+        assert str(tns) == "[[1 2]\n [3 4]]"
 
     @attr('cuda')
     @nottest  # TODO: fix this comparison
