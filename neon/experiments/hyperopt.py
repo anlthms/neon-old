ceffb35cc943aee6dd48163aa7a30212c2c9316c
Urs <urs@nervanasys.com>
2014-12-23 15:04:22 -0800

    HyperOpt switched to manual line by line parsing as suggested by @scott

--- neon/experiments/hyperopt.py
+++ neon/experiments/hyperopt.py
@@ -21,16 +21,37 @@ from ipdb import set_trace as trace
 
 logger = logging.getLogger(__name__)
 
+def parse_line(line):
+    """
+    move this function into hyperopt
+    """
+    # example: 0                  1              2         3       4
+    # ['learning_rate:', '!hyperopt:shotgun', '{float,', '0.1,', '1.0},', '#', 'range', 'for', 'HYPEROPT.']
+    dic = [k.strip("{},") for k in line.split()]
+    # check sanity
+    if dic[2] != 'float' and dic[2] != 'int' and dic[2] != 'cat':
+        raise NameError('BadParameterError')
 
-def get_parameters(res):
+    ho = dict()
+    ho['chooser'] = dic[1].split(':')[1]
+    ho['type'] = dic[2]
+    ho['end'] = float(dic[3])
+    ho['start'] = float(dic[4])
+
+    out = get_parameters(ho)
+
+    return dic[0]+" "+str(out)+",\n"
+
+def get_parameters(ho):
     """
     cast range to number:
     this function is a placeholder for calls to spearmint
 
     (this should be wrapped in a class at some point)
     """
-    if res['chooser'] == 'shotgun':
-        out = (res['end'] - res['start']) * np.random.rand() + res['start']
+    if ho['chooser'] == 'shotgun':
+        out = (ho['end'] - ho['start']) * np.random.rand() + ho['start']
+        print "----BURNING IN PARAMETERS", out, "-------"
     else:
         out = None
         raise NotImplementedError('Unknown chooser for hyperpotimization')
