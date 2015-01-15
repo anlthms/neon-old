1f6656270b7aa6b070263a67eb0efb6365ccdc75
Urs <urs@nervanasys.com>
2015-01-14 16:26:17 -0800

    spearmint hyperopt for iris

--- neon/spearmint/config.pb
+++ neon/spearmint/config.pb
@@ -1,48 +1,34 @@
 language: PYTHON
-name:     "run"
+name:     "spear_wrapper"
 
-# step size .001 or .002
-# variable {
-#  name: "steps"
-#  type: FLOAT
-#  size: 2
-#  min:  1 # 1 # 0.5
-#  max:  4 # 4 # 5
-# }
 
-# momentum
-# variable {
-#  name: "momenta"
-#  type: FLOAT
-#  size: 1
-#  min:  0.5
-#  max:  0.99
-# }
+# In this simple example, autotune one stepsize and two nin parameters.
 
-# epochs
+variable {
+ name: "stepsize"
+ type: FLOAT
+ size: 1
+ min:  0.1
+ max:  1.0
+}
+
+
 variable {
  name: "epochs"
  type: INT
  size: 1
- min:  20 # must be gib enough to reach a safe point I guess
- max:  200 # 135
+ min:  100
+ max:  200
 }
 
-#dicretness
-variable {
- name: "q_weights" # going from -5:3 to -5:-2
- type: INT
- size: 5
- min:  -6
- max:  0
-}
+
 
 variable {
- name: "q_acts"  # going from 6:14 to 4:10
+ name: "numberneurons"  #
  type: INT
- size: 5
- min:  8
- max:  13
+ size: 1
+ min:  2
+ max:  20
 }
 
 # variable {
---
+++ neon/spearmint/spear_wrapper.py
@@ -0,0 +1,102 @@
+"""
+spearmint script: config.pb mentions name: "spear_wrapper" so spearmint calls
+into this file's main() function with the current set of parameters. It does:
+- read the iris hyper-yaml file
+- parse the parameters suggested by sparemint
+- generate a temp yaml file
+- run neon
+- get the outputs
+"""
+
+import os
+#os.chdir('/home/local/code/cuda-convnet2/')
+import sys
+import time
+#sys.path.append('/home/local/code/cuda-convnet2/')
+from ipdb import set_trace as trace
+
+def main(job_id, params):
+    print 'main: job #:', str(job_id)
+    print "main: we are in directory: ", os.getcwd()
+    print "main: params are: ", params
+
+    return call_convnet(params)
+
+def call_convnet(params): #
+    """
+    runs the system call to neon and reads the result to give back to sm
+    """
+
+    params['mom'] = 0.9
+    timestring = str(int(time.time()))
+
+    # Part 1: generate the yaml ifle
+    print "call_convnet: parsing yaml"
+    input_file = '/Users/urs/code/neon/examples/hyper_iris_cpu_mlp-4-2-3.yaml'
+    output_file = 'yamels/temp'+timestring+'.yaml'
+    log_file = 'log_it.txt'
+    write_params(input_file, output_file, log_file, params)
+
+    # Part 2: Run a model
+    print "call_convnet: running neon"
+    basepath = '/Users/urs/code/neon/'
+    callstring = basepath + "bin/neon " + output_file
+    os.system(callstring)
+
+
+    # Part 3: Read the model output
+    print "call_convnet: writing outputs"
+    modelname =  sorted(os.listdir("./output/ConvNet_spearmint_" + timestring))[-1] # this is a problem:
+    model = IGPUModel.load_checkpoint("./output/ConvNet_spearmint_" + timestring + "/" + modelname)
+    print model['model_state'].keys()
+    result = model['model_state']['test_outputs'][-1][0]['logprob'][1]
+    return result/10000.
+
+def write_params(input_file, output_file, log_file, params):
+    """
+    go thorugh the hyperyaml line by line, fill in proposed values from sm
+    and write line by line to tempyaml
+    [TODO] right now this is a hardcoded set of parameters. Ultimately we
+           want to adapt this to what the yaml is "requesting" from sm.
+    """
+    mom =  params['mom']  # 0.9 hardcoded above
+    nin1, nin2 = params['numberneurons'][0], params['numberneurons'][1]
+    step = params['stepsize']
+
+    # read all lines from source
+    with open(input_file, 'r') as f:
+        # write all lines to target
+        with open(output_file, 'w') as g:
+            # append to log file
+            with open(log_file, 'a') as h:
+                for line in f:
+                    if 'hyperopt' in line:
+                        print "write_params: trying to set a trace"
+                        line = parse_line(line, params)
+                        print "line:", line
+                        h.write(": " + line)
+                    # write all lines, but parse ones with hyperopt.
+                    g.write(line)
+
+def parse_line(line, params):
+    """
+    This function should strip off the "hyperopt" and instead place in a
+    value
+    """
+    dic = [k.strip("{},") for k in line.split()]
+    print "parseline: split into", dic
+    # check sanity
+    if (dic[2] != 'numberneurons') and (dic[2] != 'stepsize')and (dic[2] != 'epochs'):
+        # want number_neurons and learning_rates, nothing else is supported.
+        raise NameError('UrsIsNotCoolWithThisError')
+
+    # ho = dict()
+    # ho['chooser'] = dic[1].split(':')[1]
+    # ho['type'] = dic[2]
+    # ho['end'] = float(dic[3])
+    # ho['start'] = float(dic[4])
+    # print "parseline: ho", ho
+
+    out = params[dic[2]][0] # need to figure out which one to pick here!
+
+    return dic[0] + " " + str(out) + ",\n"
