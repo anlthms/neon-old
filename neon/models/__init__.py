1c1c9fc76d4286de224737ad1f192dfb93f38a3d
Scott Leishman <scott@nervanasys.com>
2015-02-08 22:15:58 -0800

    further matplotlib requirements.

--- neon/models/__init__.py
+++ neon/models/__init__.py
@@ -1,10 +1,13 @@
 # import shortcuts
-from neon.models.autoencoder import Autoencoder  # noqa
-from neon.models.balance import Balance  # noqa
-from neon.models.dbn import DBN  # noqa
-from neon.models.gb import GB  # noqa
-from neon.models.mlp import MLP  # noqa
-from neon.models.rbm import RBM  # noqa
-from neon.models.rnn import RNN  # noqa
-from neon.models.gb_dist import GBDist  # noqa
-from neon.models.convnet_dist import ConvnetDist  # noqa
+try:
+    from neon.models.autoencoder import Autoencoder  # noqa
+    from neon.models.balance import Balance  # noqa
+    from neon.models.dbn import DBN  # noqa
+    from neon.models.gb import GB  # noqa
+    from neon.models.mlp import MLP  # noqa
+    from neon.models.rbm import RBM  # noqa
+    from neon.models.rnn import RNN  # noqa
+    from neon.models.gb_dist import GBDist  # noqa
+    from neon.models.convnet_dist import ConvnetDist  # noqa
+except ImportError:
+    pass
