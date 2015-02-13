f63709e2b2fd4402eaefbca405a36e859f2c9d09
Scott Leishman <scott@nervanasys.com>
2015-02-13 00:36:14 -0800

    Correct deserialization.  Closes MYL-178.
    Also had to update overwrite_params to ensure checkpoint serialization can
    correctly pickup where left off.
    
    Note that one cannot currently change backends when deserializing existing
    objects.

--- neon/util/persist.py
+++ neon/util/persist.py
@@ -37,13 +37,35 @@ def ensure_dirs_exist(path):
     return path
 
 
+def convert_scalar_node(val):
+    """
+    Helper to extract and return the appropriately types value of a ScalarNode
+    object.
+
+    Arguments:
+        val: (yaml.nodes.ScalarNode): object to extract value from
+
+    Returns:
+        float, int, string: the actual value
+    """
+    if not isinstance(val, yaml.nodes.ScalarNode):
+        return val
+    if val.tag.endswith("int"):
+        return int(val.value)
+    elif val.tag.endswith("float"):
+        return float(val.value)
+    else:
+        # assume a string
+        return val.value
+
+
 def extract_child_node_vals(node, keys):
     """
     Helper to iterate through the immediate children of the yaml node object
     passed, looking for the key values specified.
 
     Arguments:
-        node (yaml.nodes.Node): the parent node upon which to being the search
+        node (yaml.nodes.Node): the parent node upon which to begin the search
         keys (list): set of strings indicating the child keys we want to
                      extract corresponding values for.
 
@@ -57,13 +79,13 @@ def extract_child_node_vals(node, keys):
         # node, and the second can be other types of nodes.
         tag = child[0].value
         if isinstance(child[1], yaml.nodes.ScalarNode):
-            val = child[1].value
+            val = convert_scalar_node(child[1])
         elif isinstance(child[1], yaml.nodes.SequenceNode):
-            val = [x.value for x in child[1].value]
+            val = [convert_scalar_node(x) for x in child[1].value]
         elif isinstance(child[1], yaml.nodes.MappingNode):
             val = dict()
             for item in child[1].value:
-                val[item[0].value] = item[1].value
+                val[item[0].value] = convert_scalar_node(item[1])
         else:
             logger.warning("unknown node type: %s, ignoring tag %s",
                            str(type(child[1])), tag)
@@ -169,11 +191,14 @@ def obj_multi_constructor(loader, tag_suffix, node,
             logger.warning("Unable to construct '%s' instance.  Error: %s",
                            cls.__name__, e.message)
             res = None
-    if res is not None:
-        # overwrite any parameters needing updated from original yaml file
-        for param in child_vals[overwrite_param]:
-            if child_vals[param] is not None:
-                res.__dict__[param] = child_vals[param]
+    if res is not None and child_vals[overwrite_param] is not None:
+        # overwrite parameters needing updates from original yaml file
+        overwrite_vals = extract_child_node_vals(node, child_vals[
+                                                 overwrite_param])
+        for param in overwrite_vals:
+            logger.info("overwriting: %s, with YAML val: %s", param,
+                        str(overwrite_vals[param]))
+            res.__dict__[param] = overwrite_vals[param]
     return res
 
 
@@ -242,6 +267,8 @@ def serialize(obj, save_path):
     See Also:
         deserialize
     """
+    if save_path is None or len(save_path) == 0:
+        return
     logger.warn("serializing %s to: %s", str(obj), save_path)
     ensure_dirs_exist(save_path)
     pickle.dump(obj, open(save_path, 'wb'), -1)
