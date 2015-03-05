351a9bfad28f924730f31177e5dec3f768895ff0
Urs Koster <urs.koster@gmail.com>
2015-03-05 13:35:14 -0800

    checkpointing for work on conv wrappers

--- neon/backends/max.py
+++ neon/backends/max.py
@@ -73,7 +73,12 @@ class MAX(Backend):
     @st.record_flops(mult=2, shape_list=st.shapes['fprop_fc'], func_name='fprop_fc')
     def fprop_fc(self, out, inputs, weights, layer=None):
         """
-        Original dot
+        Dot calls for fully conneted layer fprop, bprop and update.
+        Inputs:
+            out
+            inputs
+            weights
+
         """
         self.nl.dot(weights, inputs, out)
 
@@ -91,7 +96,108 @@ class MAX(Backend):
         """
         self.nl.dot(deltas, inputs.T, out)
 
-    @st.record_flops_ew(mult=4, arg_pos=0, func_name='ew')
+
+
+    def fprop_conv(self, out, inputs, weights, ofmshape, ofmsize, ofmlocs,
+                   ifmshape, links, nifm, padding, stride, ngroups, fpropbuf,
+                   local=False):
+        """
+        Forward propagate the inputs of a convolutional network layer to
+        produce output pre-activations (ready for transformation by an
+        activation function).
+
+        Arguments:
+            out (GPUTensor): Where to store the forward propagated results.
+            inputs (GPUTensor): Will be either the dataset input values (first
+                             layer), or the outputs from the previous layer.
+            weights (GPUTensor): The weight coefficient values for this layer.
+            ofmshape (tuple): Dimensions of each output feature map (typically
+                              number of height and width neurons).
+            ofmsize (int): Total size of each output feature map.
+            ofmlocs (GPUTensor): Indices giving the location of each element in
+                                 each output feature map stored in out.
+            ifmshape (tuple): Dimensions of each input feature map (typically
+                              number of height and width neurons).  For this
+                              backend we expect these values to be square.
+            links (GPUTensor): Input receptive field indices.
+            nifm (int): Total number of input feature maps.
+            padding (int): Number of additional elements to include along each
+                           dimension of each local receptive field during the
+                           convolution operation.
+            stride (int): Number of neurons to shift the filter at each step.
+            ngroups (int): Number of groups.
+            fpropbuf (GPUTensor): Temporary storage buffer used to hold the
+                                  convolved outputs for a single receptive
+                                  field.  Not used for this backend.
+            local (bool, optional): Whether to do local filtering (True) or
+                                    convolution (False, the default)
+        """
+        import pdb; pdb.set_trace()
+        assert ifmshape[-2] == ifmshape[-1]
+        # convert parameters to SG convention
+        N
+        C = nimf
+        H,W = ifmshape
+        K = nofm
+        R,S = 0,0
+
+        # set up a ConvLayer parameter object
+        conv = self.nl.conv_layer(self,
+            N, C, K,
+            D=1, H=1, W=1,
+            T=1, R=1, S=1,
+            pad_d=0, pad_h=0, pad_w=0,
+            str_d=1, str_h=1, str_w=1,
+            scl_d=1, scl_h=1, scl_w=1)
+        '''
+        N: Number of images in mini-batch
+        C: Number of input feature maps
+        K: Number of output feature maps
+
+        D: Depth  of input image
+        H: Height of input image
+        W: Width  of input image
+
+        T: Depth  of filter kernel
+        R: Height of filter kernel
+        S: Width  of filter kernel
+        '''
+        self.nl.fprop_conv(conv, I, F, O, alpha=1.0, repeat=1)
+        # cudanet.convolution(
+        #     weights._tensor, inputs._tensor, out._tensor,
+        #     ifmshape[-2], ofmshape[-2], ofmshape[-1], padding, stride, nifm,
+        #     ngroups)
+
+    def bprop_conv(self, out, weights, deltas, ofmshape, ofmsize, ofmlocs,
+                   ifmshape, links, padding, stride, nifm, ngroups, bpropbuf,
+                   local=False):
+        """
+        Backward propagate the error through a convolutional network layer.
+        """
+        bprop_conv(conv, F, E, grad_I, alpha=1.0, repeat=1)
+        # cudanet.deconvolve_errors(
+        #     weights._tensor, deltas._tensor,
+        #     out._tensor, ifmshape[-2], ifmshape[-1], ofmshape[-2],
+        #     padding, stride, nifm, ngroups)
+
+    def update_conv(self, out, inputs, weights, deltas, ofmshape, ofmsize,
+                    ofmlocs, ifmshape, links, nifm, padding, stride, ngroups,
+                    fwidth, updatebuf, local=False, layer=None):
+        """
+        Compute the updated gradient for a convolutional network layer.
+
+        """
+        self.nl.update_conv(conv, I, E, grad_F, alpha=1.0, repeat=1)
+        # cudanet.deconvolve_wts(
+        #     deltas._tensor, inputs._tensor, out._tensor,
+        #     ifmshape[-2], ofmshape[-2], ofmshape[-1], fwidth,
+        #     padding, stride, nifm, ngroups, ofmshape[-2], local)
+
+
+
+
+
+    @st.record_flops_ew(mult=4, arg_pos=0, func_name='ew_sig')
     def logistic(self, x, out):
         self.nl.sig(x, out=out)
         # self.multiply(x, -1.0, out=out)
@@ -100,7 +206,7 @@ class MAX(Backend):
         # self.reciprocal(out, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew_relu')
     def rectlin(self, x, out):
         # x and out are the same buffer
         self.nl.maximum(x, 0., out=out)
@@ -144,19 +250,19 @@ class MAX(Backend):
         return FloatArray(ary.shape, dtype, allocator=allocator, name=name,
                           rounding=self.nl.round_mode).set(ary)
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew_add')
     def add(self, left, right, out):
         """assignment"""
         self.nl.add(left, right, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=1, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=1, func_name='ew_sub')
     def subtract(self, left, right, out):
         """assignment"""
         self.nl.subtract(left, right, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew_mul')
     def multiply(self, left, right, out):
         """assignment"""
         self.nl.multiply(left, right, out=out)
@@ -168,31 +274,31 @@ class MAX(Backend):
         self.nl.divide(left, right, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew_gre')
     def greater(self, left, right, out):
         """assignment"""
         self.nl.greater(left, right, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew_neq')
     def not_equal(self, left, right, out):
         """assignment"""
         self.nl.not_equal(left, right, out=out)
         return out
 
-    @st.record_flops_ew(mult=2, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=2, arg_pos=0, func_name='ew_clip')
     def clip(self, a, a_min, a_max, out):
         """assignment"""
         self.nl.clip(a, a_min, a_max, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew')
+    @st.record_flops_ew(mult=1, arg_pos=0, func_name='ew_log')
     def log(self, a, out):
         """assignment"""
         self.nl.log(a, out=out)
         return out
 
-    @st.record_flops_ew(mult=1, arg_pos=0, func_name='reduce')
+    #@st.record_flops_ew(mult=1, arg_pos=0, func_name='reduce')
     def argmax(self, a, out, axis=1):
         """assignment"""
         self.nl.argmax(a, out=out, axis=axis)
--- neon/diagnostics/speed_plots.py
+++ neon/diagnostics/speed_plots.py
@@ -15,11 +15,13 @@ import numpy as np
 def print_performance_stats(backend, logger):
 
 
-    call_list = ['fprop_fc', 'bprop_fc', 'update_fc', 'ew',
-                 'reduce', 'sum', 'logistic' ]
+    call_list = backend.flop_dict.keys()
     used_call_list = []
+    timed_calls = []
+    timed_times = []
     total_time = 0
     total_gflop = 0
+    num_bins = 30
     for call in call_list:
         logger.info("Performed %2.2f GFLOP \tin %2.2fs \t(%d %s calls)",
                     sum(backend.flop_dict[call])/ 1e9,
@@ -33,15 +35,18 @@ def print_performance_stats(backend, logger):
         total_time += time_array.sum()
         total_gflop += gflop_array.sum()
         flop_per_s = gflop_array / time_array  # in GFLOP/s
-        num_bins = 50
-        #import pdb; pdb.set_trace()
-        if len(gflop_array) > 1:
+        # plot only the biggest contributors
+        if time_array.sum() > .2:
             used_call_list.append(call)
-            plt.figure(1)
-            n, bins, patches = plt.hist(flop_per_s, num_bins,
-                                        weights=time_array, #flop_array/ 1e9,
-                                        range=(0, 5000),
-                                        normed=0, alpha=0.5)
+            timed_calls.append(flop_per_s)
+            timed_times.append(time_array)
+    #import pdb; pdb.set_trace()
+    plt.figure(1)
+    n, bins, patches = plt.hist(timed_calls, num_bins,
+                                weights=timed_times, #flop_array/ 1e9,
+                                range=(0, 5000),
+                                histtype='barstacked',
+                                normed=0, alpha=0.5)
 
 
     plt.title(r'Time vs. Compute, total %2.2fs %2.2fGF average %2.2fGFLOP/S'  % (total_time, total_gflop, total_gflop/total_time))
@@ -51,6 +56,6 @@ def print_performance_stats(backend, logger):
     plt.ylabel('time / s')
     plt.legend(used_call_list)
     plt.show()
-    #import pdb; pdb.set_trace()
+    #
     # TODO: Within a bin, figure out how many FLOP were performed, or how many seconds were spend.
 
