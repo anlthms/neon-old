8711a7f56861aa9acb7f79f6a700540f3b57edb1
Will Constable <will@nervanasys.com>
2015-04-14 16:56:31 -0700

    work in progress- add LoadDecodeThread to prepare a second macrobatch while the current macrobatch is processed; convert labels to one hot encoding in macrobatch (incomplete, need to finish this part)

--- neon/datasets/imageset.py
+++ neon/datasets/imageset.py
@@ -36,15 +36,19 @@ class DeviceTransferThread(Thread):
         return
 
     def run(self):
+
+        b_idx = self.ds.active_buf_idx
         s_idx = self.ds.mini_idx * self.ds.batch_size
         e_idx = s_idx + self.ds.batch_size
         d_idx = (self.ds.batches_generated + 1) % 2
+        # print "DTT active buf ", b_idx, "minibatch ", self.ds.mini_idx
+
         # print "Putting into ", d_idx, self.ds.batches_generated
         # Host versions of each var
-        h_img = self.ds.img_macro[s_idx:e_idx]
-        h_lbl = {k: self.ds.lbl_macro[k][s_idx:e_idx]
+        h_img = self.ds.img_macro[b_idx][s_idx:e_idx]
+        h_lbl = {k: self.ds.lbl_macro[b_idx][k][s_idx:e_idx]
                  for k in self.ds.label_list}
-        h_tgt = None if self.ds.tgt_macro is None else self.ds.tgt_macro[s_idx:e_idx]
+        h_tgt = None if self.ds.tgt_macro[b_idx] is None else self.ds.tgt_macro[b_idx][s_idx:e_idx]
 
         d_img = self.ds.inp_be[d_idx]
         d_lbl = self.ds.lbl_be[d_idx]
@@ -52,6 +56,66 @@ class DeviceTransferThread(Thread):
         DeviceTransferThread.transpose_and_transfer(h_img, h_lbl, h_tgt,
                                                     d_img, d_lbl, d_tgt,
                                                     self.ds.backend)
+
+
+class LoadDecodeThread(Thread):
+    """
+    Load and decode a macrobatch of images in a separate thread.
+
+    Double-buffer macrobatch data structures. use ds.decode_buf_idx 
+
+
+    """
+
+    def __init__(self, ds):
+        Thread.__init__(self)
+        self.ds = ds
+
+
+    def run(self):
+
+        bsz = self.ds.batch_size * self.ds.backend.par.size()
+        jdict = self.ds.get_macro_batch()
+        mac_sz = len(jdict['data'])
+        b_idx = self.ds.decode_buf_idx
+
+
+        # print "LoadDecodeThread start b_idx ", b_idx
+        self.ds.tgt_macro[b_idx] = jdict['targets'] if 'targets' in jdict else None
+        self.ds.lbl_macro[b_idx] = {k: jdict['labels'][k] for k in self.ds.label_list}
+
+        for lbl in self.ds.label_list:
+            eye_buf = np.eye(self.ds.nclass)
+            """
+
+            need to understand the right data layout for transferring to gpu.
+
+            """
+            self.ds.lbl_macro1hot[b_idx][lbl] = eye_buf[self.ds.lbl_macro[b_idx][lbl]]
+
+        
+        # print self.ds.lbl_macro1hot[b_idx]
+        imgworker.decode_list(jpglist=jdict['data'],
+                              tgt=self.ds.uimg_macro[b_idx][:mac_sz],
+                              orig_size=self.ds.output_image_size,
+                              crop_size=self.ds.cropped_image_size,
+                              center=self.ds.predict, flip=True,
+                              rgb=self.ds.rgb,
+                              nthreads=self.ds.num_workers)
+        mean_val = 127
+        if self.ds.mean_norm:
+            mean_val = self.ds.mean_crop.reshape((1, self.ds.npixels))
+        np.subtract(self.ds.uimg_macro[b_idx], mean_val, self.ds.img_macro[b_idx])
+        if mac_sz < self.ds.macro_size:
+            self.ds.img_macro[mac_sz:] = 0
+        self.ds.minis_per_macro = mac_sz / bsz
+
+        # print "LoadDecodeThread finish b_idx ", b_idx
+        import sys
+        sys.stdout.flush()
+
+        return
+
 
 class Imageset(Dataset):
 
@@ -86,7 +150,7 @@ class Imageset(Dataset):
 
         self.__dict__.update(kwargs)
         req_param(self, ['cropped_image_size', 'output_image_size',
-                         'imageset', 'save_dir', 'repo_path', 'macro_size'])
+                         'imageset', 'save_dir', 'repo_path', 'macro_size', 'nclass'])
 
         opt_param(self, ['image_dir'], os.path.join(self.repo_path,
                                                     self.imageset))
@@ -171,11 +235,20 @@ class Imageset(Dataset):
         # self.mini_idx = self.minis_per_macro - 1
 
         # Allocate space for the host and device copies of input
+
+        self.decode_buf_idx = 0
+        self.num_decode_buf = 2
+        self.decoder_thread = None
+
+        self.tgt_macro = [None for i in range(self.num_decode_buf)]
+        self.lbl_macro = [None for i in range(self.num_decode_buf)]
+        self.lbl_macro1hot = [{} for i in range(self.num_decode_buf)]
         inp_macro_shape = (self.macro_size, self.npixels)
         inp_shape = (self.npixels, self.batch_size)
-        self.uimg_macro = np.zeros(inp_macro_shape, dtype=np.uint8)
-        self.img_macro = np.zeros(inp_macro_shape, dtype=np.int8)
+        self.uimg_macro = [np.zeros(inp_macro_shape, dtype=np.uint8) for i in range(self.num_decode_buf)]
+        self.img_macro = [np.zeros(inp_macro_shape, dtype=np.int8) for i in range(self.num_decode_buf)]
         self.inp_be = [sbaf(inp_shape, dtype=btype) for i in range(2)]
+
 
         # Allocate space for device side labels
         lbl_shape = (1, self.batch_size)
@@ -189,34 +262,49 @@ class Imageset(Dataset):
         self.data = [
             [self.inp_be[i], self.tgt_be[i], self.lbl_be[i]] for i in range(2)]
         self.d_idx = 0
+
+
+
         return num_batches
 
+# could keep decoding macrobatches as fast as possible if more than 2 buffers are available
+
     def start_loader(self, batch_idx):
-        bsz = self.batch_size * self.backend.par.size()
+
+        """
+        need to decode a macrobatch up front before starting minibatch processing
+
+        want to have second macrobatch decode running in parallel with minibatch decoding
+
+        can't start another macrobatch until minibatch idx == 0 again freeing up the dbl buf
+
+        """
+        # print "start_loader batch_idx", batch_idx
         if batch_idx == 0:
-            jdict = self.get_macro_batch()
-            mac_sz = len(jdict['data'])
-            self.tgt_macro = jdict['targets'] if 'targets' in jdict else None
-            self.lbl_macro = {k: jdict['labels'][k] for k in self.label_list}
-            imgworker.decode_list(jpglist=jdict['data'],
-                                  tgt=self.uimg_macro[:mac_sz],
-                                  orig_size=self.output_image_size,
-                                  crop_size=self.cropped_image_size,
-                                  center=self.predict, flip=True,
-                                  rgb=self.rgb,
-                                  nthreads=self.num_workers)
-            mean_val = 127
-            if self.mean_norm:
-                mean_val = self.mean_crop.reshape((1, self.npixels))
-            np.subtract(self.uimg_macro, mean_val, self.img_macro)
-            if mac_sz < self.macro_size:
-                self.img_macro[mac_sz:] = 0
-            self.minis_per_macro = mac_sz / bsz
+
+            if self.decoder_thread != None:
+                #no-op unless minibatch loading finished faster than macrobatch in bg thread
+                self.decoder_thread.join()
+            else:
+                # special case for first run through
+                self.decoder_thread = LoadDecodeThread(self)
+                self.decoder_thread.start()
+                self.decoder_thread.join()
+
+            # usual case for kicking off a background macrobatch thread
+            self.active_buf_idx = self.decode_buf_idx
+            self.decode_buf_idx = (self.decode_buf_idx + 1) % self.num_decode_buf
+            self.decoder_thread = LoadDecodeThread(self)
+            self.decoder_thread.start()
+
 
         self.loader_thread = DeviceTransferThread(self)
         self.loader_thread.start()
         self.loader_thread.join()
         self.batches_generated += 1
+        # print "start_loader generated batches: ", self.batches_generated
+        import sys
+        sys.stdout.flush()
 
     def get_data_from_loader(self):
         next_mini_idx = (self.mini_idx + 1) % self.minis_per_macro
