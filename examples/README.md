dc9c7dfe75eb78454e8da988fc4e9e1c9e31b26d
Scott Leishman <scott@nervanasys.com>
2015-05-01 21:19:00 -0700

    Update license preamble in all files.

--- examples/README.md
+++ examples/README.md
@@ -3,7 +3,7 @@ This directory contains some sample neon experiment configuration files.
 The general process for running these is to first install neon into your
 system from the top-level directory via:
 
-    make install
+    sudo make install
 
 Which should yield an executable in your system path named `neon`
 
--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # ----------------------------------------------------------------------------
 # Copyright 2014 Nervana Systems Inc.  All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # ----------------------------------------------------------------------------
 
 import os
@@ -23,7 +34,24 @@ except:
 
 if write_version:
     txt = "# " + ("-" * 77) + "\n"
-    txt += "# " + "Copyright 2014 Nervana Systems Inc. All rights reserved.\n"
+    txt += "# Copyright 2014 Nervana Systems Inc.  All rights reserved.\n"
+    txt += "# Licensed under the Apache License, Version 2.0 "
+    txt += "(the \"License\");\n"
+    txt += "# you may not use this file except in compliance with the "
+    txt += "License.\n"
+    txt += "# You may obtain a copy of the License at\n"
+    txt += "#\n"
+    txt += "#      http://www.apache.org/licenses/LICENSE-2.0\n"
+    txt += "#\n"
+    txt += "# Unless required by applicable law or agreed to in writing, "
+    txt += "software\n"
+    txt += "# distributed under the License is distributed on an \"AS IS\" "
+    txt += "BASIS,\n"
+    txt += "# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or "
+    txt += "implied.\n"
+    txt += "# See the License for the specific language governing permissions "
+    txt += "and\n"
+    txt += "# limitations under the License.\n"
     txt += "# " + ("-" * 77) + "\n"
     txt += "\"\"\"\n%s\n\"\"\"\nVERSION = '%s'\nSHORT_VERSION = '%s'\n"
     fname = os.path.join(os.path.dirname(__file__), 'neon', 'version.py')
