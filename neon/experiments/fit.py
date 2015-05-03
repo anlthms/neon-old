98d9fe481211b9b7cc4c7ca8413be27d253489d0
Anil Thomas <anil@nervanasys.com>
2015-05-03 13:06:02 -0700

    Check if attribute exists, before testing.

--- neon/experiments/fit.py
+++ neon/experiments/fit.py
@@ -58,7 +58,8 @@ class FitExperiment(Experiment):
         self.dataset.set_batch_size(self.model.batch_size)
         self.dataset.backend = self.backend
         self.dataset.load()
-        if self.dataset.serialized_path is not None:
+        if hasattr(self.dataset, 'serialized_path') and (
+                self.dataset.serialized_path is not None):
             logger.warning('Ability to serialize dataset has been deprecated.')
 
         # fit the model to the data, save it if specified
